e5854116f074ca361adb4d0eb796c8e87550e0b5
Abdul Rehman <arsarsars1@gmail.com>
2024-05-20 23:02:58 +0500

    test removed

--- test/mocks/mock_api_client.dart
+++
@@ -1,4 +0,0 @@
-import 'package:mocktail/mocktail.dart';
-import 'package:task_manager/services/api_client.dart';
-
-class MockApiClient extends Mock implements ApiClient {}
--- test/mocks/mock_authentication_service.dart
+++
@@ -1,4 +0,0 @@
-import 'package:mocktail/mocktail.dart';
-import 'package:task_manager/services/authentication_service.dart';
-
-class MockAuthenticationService extends Mock implements AuthenticationService {}
